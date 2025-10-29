c5d62f94bb7d15c345fd2bb6a3fca97265298477
Nina1333 <ninabanan10@gmail.com>
2025-10-29 15:06:41 +0100

    Komentarz

---
+++ 1-pierwszyprogram/c++zadanie2!.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ int main(){
+ int a = 3
+  cout << "Liczba a: " << a;
+ int b = 2
+  cout << "Liczba b: " << b;
+ wynik1 = a + b
+  cout << "Suma: " << wynik1;
+ wynik2 = a - b
+  cout << "Różnica: " << wynik2;
+ wynik3 = a * b
+  cout << "Iloczyn: " << wynik3;
+    return 0;
+}
---
+++ 1-pierwszyprogram/c++zadanie3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a = 45;
+	cout << "Zmienna calkowita:" << a << endl;
+	float b = 2.998467;
+	cout << "zmienna zmiennoprzecinkowa:" << b << endl;
+	char c = '&';
+	cout << "zmienna znakowa:" << c << endl;
+	bool d = true;
+	cout << "zmienna logiczna:" << d << endl;
+	
+	
+    return 0;
+}

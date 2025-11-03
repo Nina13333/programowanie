97077d4838423b123dccec99d564dca667fdd0cb
Nina1333 <ninabanan10@gmail.com>
2025-11-03 14:10:20 +0100

    Komentarz

---
+++ 2/Zadanie1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int liczba;
+	cout << "Podaj liczbe: ";
+	cin >> liczba;
+	// Operator AND (&&)
+	if ((liczba >=0) && (liczba <=49)) {
+		cout << "Ocena niedostateczna" << endl;
+	
+	} else if ((liczba >=50) && (liczna <=69)) {
+	} cout << ""
+    return 0;
+}

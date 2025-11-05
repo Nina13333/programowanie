079bff27c6ae89d06cac652db5f2e43a3749c043
Nina1333 <ninabanan10@gmail.com>
2025-11-05 15:05:39 +0100

    Komentarz

--- 2/Zadanie1.cpp
+++ 2/Zadanie1.cpp
@@ -8,7 +8,18 @@ int main(){
 	if ((liczba >=0) && (liczba <=49)) {
 		cout << "Ocena niedostateczna" << endl;
 	
-	} else if ((liczba >=50) && (liczna <=69)) {
-	} cout << ""
+	} else if ((liczba >=50) && (liczba <= 69)) {
+		cout << "Ocena dostateczna" << endl;
+	
+	} else if ((liczba >=70) && (liczba <=84)) {
+		cout << "Ocena dobra" << endl;
+		
+	} else if ((liczba >=85) && (liczba <=99)) {
+		cout << "Ocena bardzo dobra" << endl;
+		
+	} else if (liczba ==100) {
+		cout << "Ocena celujaca" << endl;
+	}
+		
     return 0;
 }
---
+++ 2/zadnia2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int liczba;
+	cout << "Podaj wage"
+	cin >> liczba;
+	// Operator OR (||)
+	}if (liczba <=18.5) {
+		cout << "Niedowaga"
+	
+	}else if ((liczba >=18.5) || (liczba <=24.9)) {
+		cout << "W normie"	
+		
+	}else if ((li))
+	}
+    return 0;
+}
